a4b9ae600db5249e4d3faa42201a5ff625ed2961
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-06-04 08:15:21 -0500

    I got the value to go to the database. I think there's a bug in it where it will sometimes change the rating of other movies. will require more testing to meet standard.
    still working on the function to load the rating from the DB and display it. also need to figure out what is going on with the bubbling and write a permanent fix for it.

--- src/arrayLoader.js
+++ src/arrayLoader.js
@@ -9,15 +9,17 @@ const pushObj = {
     id:'',
     poster:'',
     plot:'',
+    userRating:'',
 };
 let newMovie = Object.create(pushObj);
 
 function dbChecker() {
 
-   return getMovies().then( (movies)=> {
+   return getMovies().then((movies)=> {
 
        movies.forEach( (movie)=> {
 
+            //pulls data from the OMDB api
            let title = movie.title;
            let rating = movie.rating;
            let id = movie.id;
@@ -28,7 +30,7 @@ function dbChecker() {
                type: 'movie',
                plot: 'short'
            };
-
+            //loads the newMovie object to be sent to local DB
            omdbApi.get(params, function(err, data) {
 
                if ( data === null){
@@ -42,9 +44,11 @@ function dbChecker() {
                 newMovie.plot = data.Plot;
                 newMovie.poster = data.Poster;
 
+
+               //dynamically generated movie cards.
                if(data.Ratings.length > 0) {
                    newMovie.rating = data.Ratings[0].Value;
-                   let rating = `<p class="card-text"> Rotten Tomatoes: ${data.Ratings[0].Value}</p>`;
+                   let rating = `<p class="card-text">${"Rotten Tomatoes: " + data.Ratings[0].Value}</p>`;
 
                } else {newMovie.rating = "";
                     let rating = "";
@@ -53,20 +57,20 @@ function dbChecker() {
                newMovie.plot = data.Plot;
                 mid = data.imdbID;
 
+
                let poster = `<img class="card-img-top" src="${data.Poster}" alt="Card image cap">`;
 
-               let stars = ` <div class='movie_choice'>
+               let stars = `<div class='movie_choice'>
                    <div id="${data.imdbID}" class="rate_widget">
                    <div value="1" class="star_1 ratings_stars"></div>
                    <div value="2" class="star_2 ratings_stars"></div>
                    <div value="3" class="star_3 ratings_stars"></div>
                    <div value="4" class="star_4 ratings_stars"></div>
                    <div value="5" class="star_5 ratings_stars"></div>
-                   <!--<div class="total_votes">vote data</div>-->
                </div>
                </div>`;
 
-               if(data.Poster === "N/A"){
+               if(data.poster === "N/A"){
                    poster = '';
                }
 
@@ -85,7 +89,7 @@ function dbChecker() {
 
                editMovie(newMovie);
 
-               //Handles the delete button
+               //delete button
                $(".delete").unbind("click").on("click",function() {
                    let firedButton = $(this).attr('value');
                    console.log(firedButton);
@@ -93,81 +97,45 @@ function dbChecker() {
                    $(".container").empty();
                    dbChecker();
 
-               });
+                    }
+               );
 
+               //user rating system
                let clicked = 0;
                let selectedRating = 0;
 
                $(".ratings_stars").click(
                    // Handles the click
                    function() {
+
                        $(this).prevAll().addBack().addClass('ratings_vote');
                         selectedRating = ($(this).attr('value'));
-                        clicked = 1;
-
-
-
-
+                        newMovie.userRating = selectedRating;
+                       clicked = 1;
+                       event.stopPropagation();
+                       editMovie(newMovie);
                    },
                );
                $(".ratings_stars").hover(
                    // Handles the mouseover
                    function() {
                        $(this).prevAll().addBack().addClass('ratings_over');
-                       // $(this).nextAll().removeClass('ratings_vote');
                    },
-                 //  Handles the mouseout if no option was clicked
 
                    function() {
-                   //     if(clicked === 0){
-                   //         $(this).prevAll().addBack().removeClass('ratings_over');
-                   // }else if (clicked === 1){
+
                            $(this).prevAll().addBack().removeClass('ratings_over');
-
                            $(this).nextAll().addBack().removeClass('ratings_over')
-                       // }
-
                    }
                );
 
 
-
-               // $('.rate_widget').each(function(i) {
-               //     let widget = this;
-               //     let out_data = {
-               //         widget_id : $(widget).attr('id'),
-               //         fetch: 1
-               //     };
-               //     $.post(
-               //         // 'ratings.php',
-               //         out_data,
-               //         function(INFO) {
-               //             $(widget).data( 'fsr', INFO );
-               //             set_votes(widget);
-               //         },
-               //         'json'
-               //     );
-               // });
-
-               // function set_votes(widget) {
-               //
-               //     let avg = $(widget).data('fsr').whole_avg;
-               //     let votes = $(widget).data('fsr').number_votes;
-               //     let exact = $(widget).data('fsr').dec_avg;
-               //
-               //     $(widget).find('.star_' + avg).prevAll().andSelf().addClass('ratings_vote');
-               //     $(widget).find('.star_' + avg).nextAll().removeClass('ratings_vote');
-               //     $(widget).find('.total_votes').text( votes + ' votes recorded (' + exact + ' rating)' );
-               // }
                $(".ratings_stars").click(function () {
-                    let id = $(this).parent().attr('id');
-                     let rating = $(this).attr('value')
+                       let id = $(this).parent().attr('id');
+                       let rating = $(this).attr('value')
 
-               }
-
-
+                   }
                );
-
             });
        });
    });
